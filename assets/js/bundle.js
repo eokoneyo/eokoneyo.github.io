af7a13e81e3095b03784124b230ee78d7a330330
Eyo Okon Eyo <maziey93@gmail.com>
2019-07-24 19:59:13 +0200

    start work landing page for new website

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,20 +1,32 @@
 ;(function (global) {
 
-    const logger = (...args) => console.log.apply(this, args);
-    
-    const test = logger('Hello World!');
+    document.addEventListener('DOMContentLoaded', function () {
+        const ANIMATION_DURATION = 2000;
 
-    const name = 'yo';
-    const age = 28; 
-    const displayName = 'loyal';
-
-    const val = {
-        name,
-        displayName
-    };
-
-    const valExtra = {...val, age}
-
-    return global.myApp = {};
+        const DOM = {};
+        DOM.preloader = document.querySelector('.preloader');
+        DOM.shape = DOM.preloader.querySelector('svg.shape');
+        DOM.path = DOM.shape.querySelector('path');
+        
+        setTimeout(() => {
+            anime({
+                targets: DOM.preloader,
+                duration: ANIMATION_DURATION,
+                easing: 'easeInOutSine',
+                translateY: '-200vh',
+            });
+      
+            anime({
+                targets: DOM.path,
+                duration: ANIMATION_DURATION,
+                easing: 'easeOutQuad',
+                d: DOM.path.getAttribute('pathdata:id'),
+                complete: () => {
+                  document.body.style.overflow = 'auto';
+                  DOM.preloader.style.display = 'none'
+                }
+            });
+        }, 1000);
+    });
 
 })(window);
