f09bc88fb139bcca4b424159c413e2d55baf9659
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-13 21:23:10 +0000

    Deploy eokoneyo/eokoneyo.github.io to github.com/eokoneyo/eokoneyo.github.io.git:master

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,32 +1,67 @@
 'use strict';
 
+var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }; })();
+
 ;(function (global) {
+
+    var setCookie = function setCookie(name, value) {
+        return global.document.cookie = name + '=' + value;
+    };
+
+    var getCookieValue = function getCookieValue(name) {
+        var cookieString = String(global.document.cookie);
+
+        if (cookieString.indexOf(name) < 0) return null;
+
+        var searchRegex = new RegExp(name + '=(\\w*);?');
+
+        var _cookieString$match = cookieString.match(searchRegex);
+
+        var _cookieString$match2 = _slicedToArray(_cookieString$match, 2);
+
+        var value = _cookieString$match2[1];
+
+        return value;
+    };
+
     document.addEventListener('DOMContentLoaded', function () {
         var ANIMATION_DURATION = 2000;
+        var HAS_SEEN_PRELOADER_COOKIE = 'eoe_has_seen_preloader';
 
         var DOM = {};
         DOM.preloader = global.document.querySelector('.preloader');
         DOM.shape = DOM.preloader.querySelector('svg.shape');
         DOM.path = DOM.shape.querySelector('path');
 
-        setTimeout(function () {
+        var removePreloader = function removePreloader(animationDuration) {
             anime({
                 targets: DOM.preloader,
-                duration: ANIMATION_DURATION,
+                duration: animationDuration,
                 easing: 'easeInOutSine',
                 translateY: '-200vh'
             });
 
             anime({
                 targets: DOM.path,
-                duration: ANIMATION_DURATION,
+                duration: animationDuration,
                 easing: 'easeOutQuad',
                 d: DOM.path.getAttribute('pathdata:id'),
                 complete: function complete() {
                     global.document.body.style.overflow = 'auto';
+                    // mark preloader as seen for the session
+                    setCookie(HAS_SEEN_PRELOADER_COOKIE, true);
                 }
             });
-        }, 1000);
+        };
+
+        if (getCookieValue(HAS_SEEN_PRELOADER_COOKIE) === 'true') {
+            DOM.preloader.style.display = 'none';
+            global.document.body.style.overflow = 'auto';
+        } else {
+            setTimeout(function () {
+                return removePreloader(ANIMATION_DURATION);
+            }, 1000);
+        }
 
         //Do page view tracking
         if (global.ga) {
