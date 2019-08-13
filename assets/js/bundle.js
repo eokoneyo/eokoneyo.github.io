d2b247a6b82a00d4160d52dea8f777897ee6b255
Eyo Okon Eyo <maziey93@gmail.com>
2019-08-13 23:18:48 +0200

    only show preloader on new visit not every page reload

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,31 +1,55 @@
 ;(function (global) {
+    
+    const setCookie = (name, value) => global.document.cookie = `${name}=${value}`;
+
+    const getCookieValue = name => {
+        const cookieString = String(global.document.cookie);
+
+        if(cookieString.indexOf(name) < 0) return null;
+
+        const searchRegex = new RegExp(`${name}=(\\w*);?`);
+
+        const [, value] = cookieString.match(searchRegex);
+
+        return value;
+    }
+    
     document.addEventListener('DOMContentLoaded', function () {
         const ANIMATION_DURATION = 2000;
+        const HAS_SEEN_PRELOADER_COOKIE = 'eoe_has_seen_preloader';
 
         const DOM = {};
         DOM.preloader = global.document.querySelector('.preloader');
         DOM.shape = DOM.preloader.querySelector('svg.shape');
         DOM.path = DOM.shape.querySelector('path');
-        
-        setTimeout(() => {
+
+        const removePreloader = (animationDuration) => {
             anime({
                 targets: DOM.preloader,
-                duration: ANIMATION_DURATION,
+                duration: animationDuration,
                 easing: 'easeInOutSine',
                 translateY: '-200vh',
             });
       
             anime({
                 targets: DOM.path,
-                duration: ANIMATION_DURATION,
+                duration: animationDuration,
                 easing: 'easeOutQuad',
                 d: DOM.path.getAttribute('pathdata:id'),
                 complete: () => {
-                  global.document.body.style.overflow = 'auto';
+                    global.document.body.style.overflow = 'auto';
+                    // mark preloader as seen for the session
+                    setCookie(HAS_SEEN_PRELOADER_COOKIE, true);
                 }
             });
-        }, 1000);
+        }
 
+        if(getCookieValue(HAS_SEEN_PRELOADER_COOKIE) === 'true') {
+            DOM.preloader.style.display = 'none';
+            global.document.body.style.overflow = 'auto';
+        } else {
+            setTimeout(() => removePreloader(ANIMATION_DURATION), 1000);
+        }
 
         //Do page view tracking
         if(global.ga) {
