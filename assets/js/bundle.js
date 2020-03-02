4673251486d17b1cb197fb0da86b15ef577edf8f
Eyo Okon Eyo <maziey93@gmail.com>
2020-03-02 22:59:54 +0100

    use new bundling setup

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,5 +1,7 @@
-;(function (global) {
-    
+import anime from 'animejs';
+
+((global) => {
+
     const setCookie = (name, value) => global.document.cookie = `${name}=${value}`;
 
     const getCookieValue = name => {
@@ -61,6 +63,35 @@
                 page: global.location.pathname
             });
         }
+    });
+
+
+    document.querySelector('#illustration').addEventListener('click', (evt) => {
+
+        const delay = 100;
+        const duration = 1300;
+        const easing = 'cubicBezier(0.64, 0.04, 0.35, 1)';
+
+        anime({
+            targets: '#illustration',
+            duration,
+            easing,
+            translateX: [0, -834],
+            translateY: [0, -162],
+            width: [239, 50.37],
+            height: [427, 90],
+            delay: 100
+        });
+
+        anime({
+            targets: '#info-cta',
+            duration,
+            easing,
+            delay,
+            translateX: [0, 2.5],
+            translateY: [0, -980.5]
+        });
+
     });
 
 })(window);
