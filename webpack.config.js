029b87b4c3b1e424d73ddfb13b482922d4c73261
Eyo Okon Eyo <maziey93@gmail.com>
2020-03-07 16:16:34 +0100

    switch to using webpack for compiling scss to leverage its ability to hash

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 const yaml = require('js-yaml');
 const cpy = require('cpy');
 const ManifestPlugin = require('webpack-manifest-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 let jekyllConfigFileContents;
 
@@ -17,13 +19,32 @@ const { webpack: jekyllWebpackConfig } = yaml.safeLoad(jekyllConfigFileContents)
 
 module.exports = {
     mode: process.env.JEKYLL_ENV  === 'production' ? 'production' : 'development',
-    entry: path.join(__dirname, jekyllWebpackConfig.entry),
+    entry: jekyllWebpackConfig.entry.map(entryItem => path.join(__dirname, entryItem)),
     output: {
         filename: '[name]-bundle.[contenthash].js',
         path: path.resolve(__dirname, jekyllWebpackConfig.cache_directory),
     },
     module: {
         rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {loader: 'css-loader'},
+                    {loader: 'sass-loader'}
+                ]
+            },{
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/',
+                    }
+                }]
+            },
             {
                 test: /\.js$/,
                 use: {
@@ -37,9 +58,10 @@ module.exports = {
         splitChunks: {
             cacheGroups: {
                 commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "vendor",
-                    chunks: "all"
+                    // only create vendors for file that is not a stylesheet
+                    test: /node_modules\/(?!(.*\.(sa|sc|c)ss$))/,
+                    name: 'vendor',
+                    chunks: 'all'
                 }
             }
         },
@@ -49,10 +71,14 @@ module.exports = {
     },
     plugins: [
         new ManifestPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+        new CleanWebpackPlugin(),
         function() {
             // This makes the files webpack generated available to jekyll and our jekyll plugin,
             // so the assets might be copied
-            this.plugin("done", async () => {
+            this.plugin('done', async () => {
                 await cpy(path.join(jekyllWebpackConfig.cache_directory, 'manifest.json'), path.join('_data') , {
                     rename: () => 'webpack-manifest.json'
                 })
