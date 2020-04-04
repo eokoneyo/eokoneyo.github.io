10e586f448da16918733be6e81320c7ef516dd3b
Eyo Okon Eyo <maziey93@gmail.com>
2020-04-04 22:05:55 +0200

    more work on project page look and feel

--- _plugins/dominant-color-generator.rb
+++ _plugins/dominant-color-generator.rb
@@ -1,27 +1,41 @@
 module DominantColor
-    class Generator < Jekyll::Generator
-        safe true
+  class Generator < Jekyll::Generator
+    safe true
 
-        def generate(site)
-            projectPosts = site.categories['projects']
-            projectPosts.each do |doc|
-                unless doc.data['dominantColor']
-                    doc.data['dominantColor'] = RandomColor.color_hex();
-                end
-            end
+    def generate(site)
+      projectPosts = site.categories["projects"]
+      projectPosts.each do |doc|
+        unless doc.data["dominantColor"]
+          random_color = RandomColor.new
+          doc.data["dominantColor"] = random_color.hex()
         end
-    end
-
-    # link: https://stackoverflow.com/a/58115927/4398075
-    class RandomColor
-      def self.get_random
-        rand(255)
-      end
-
-      def self.color_hex(options = {})
-        default = { red: get_random, green: get_random, blue: get_random }
-        options = default.merge(options)
-        '#%X%X%X' % options.values
       end
     end
+  end
+
+  # link: https://stackoverflow.com/a/58115927/4398075
+  class RandomColor
+    def initialize(params = {})
+      @params = params
+    end
+
+    def get_random
+      rand(255)
+    end
+
+    def get_random_color()
+      default = { red: self.get_random, green: self.get_random, blue: self.get_random }
+      _options = default.merge(@params)
+    end
+
+    def hex()
+      color = self.get_random_color()
+      "#%X%X%X" % color.values
+    end
+
+    def rgb_string()
+      color = self.get_random_color()
+      color.values.join(",")
+    end
+  end
 end
