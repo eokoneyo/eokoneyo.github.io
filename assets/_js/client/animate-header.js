814c140b2f1740f23fdb454801ec2a43c439ee3e
Eyo Okon Eyo <maziey93@gmail.com>
2020-04-18 21:50:29 +0200

    leverage modular scale for responsive typography
    improve site typography

--- assets/_js/client/animate-header.js
+++ assets/_js/client/animate-header.js
@@ -13,15 +13,17 @@ const animateHeader = (global, DOM) => {
 
   const landingPageContent = DOM.landingPage.querySelector('.main-content');
 
+  const toggleBoundary = 20;
+
   landingPage.addEventListener('scroll', throttle(() => {
     const contentRect = landingPageContent.getBoundingClientRect();
     const classIsSet = hasClass(DOM.header, 'ns-header--hidden');
 
-    if (classIsSet && contentRect.y < 20 ) {
+    if (classIsSet && contentRect.y < toggleBoundary ) {
       toggleClass(DOM.header, 'ns-header--hidden', false);
     }
 
-    if(!classIsSet && contentRect.y > 20) {
+    if(!classIsSet && contentRect.y > toggleBoundary) {
       toggleClass(DOM.header, 'ns-header--hidden', true);
     }
   }, 250));
